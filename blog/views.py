653723ddbedfb1e1e6aca256a706accd1e47b279
Ahnaf Shikder Arnob <ahnafarnob31@gmail.com>
2024-09-10 21:27:04 +0600

    After adding a single page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,25 @@
+from datetime import date
 from django.shortcuts import render
 
+posts= [
+    {
+        "slug": "ahnaf-with-juna",
+        "img": "withjuna.jpg",
+        "author": "Shamun",
+        "date": date (2024, 7 , 21),
+        "title": "Ahnaf",
+        "excerpt": "Say something about this column!",
+        "content": """ 
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis eum quam unde. Earum voluptates, laudantium dolore neque recusandae dolorum adipisci fugit labore vitae quas soluta eum exercitationem dolor magnam rem?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis eum quam unde. Earum voluptates, laudantium dolore neque recusandae dolorum adipisci fugit labore vitae quas soluta eum exercitationem dolor magnam rem?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis eum quam unde. Earum voluptates, laudantium dolore neque recusandae dolorum adipisci fugit labore vitae quas soluta eum exercitationem dolor magnam rem?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis eum quam unde. Earum voluptates, laudantium dolore neque recusandae dolorum adipisci fugit labore vitae quas soluta eum exercitationem dolor magnam rem?
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis eum quam unde. Earum voluptates, laudantium dolore neque recusandae dolorum adipisci fugit labore vitae quas soluta eum exercitationem dolor magnam rem?
+
+        """
+        
+    }
+]
 # Create your views here.
 def starting_page(request):
     return render(request, "blog/index.html")
@@ -8,5 +28,5 @@ def posts(request):
     return render(request, "blog/all-posts.html")
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request, "blog/post-details.html" )
